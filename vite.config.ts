ea87a75a0dd1aa3b3132c986fd10f93fd57ce522
Marc Ryan <ryanmq87@gmail.com>
2023-10-11 21:57:56 -0700

    Include type declarations in build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,10 +12,10 @@ export default defineConfig({
     },
     sourcemap: true,
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
+      // make sure to externalize deps that shouldn't be bundled into your library
       external: ['react', 'react-dom'],
     },
+    outDir: './dist',
   },
-  plugins: [react()],
+  plugins: [react(), dts({ entryRoot: './lib', rollupTypes: true })],
 })
