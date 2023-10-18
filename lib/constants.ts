920abdcf605d9f43d4e313fb58615e4e44db881e
Marc Ryan <ryanmq87@gmail.com>
2023-10-18 16:09:00 -0700

    Use copunctal points from a more reliable source

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,16 +1,16 @@
 import type { Vector2D, Vector3D } from './types'
 
 // Values for deutan copunctal points differs between these sources
-// https://www.color-blindness.com/2009/01/19/colorblind-colors-of-confusion
 // https://www.researchgate.net/figure/Copunctal-points-of-dichromate-confusion-lines_tbl1_228970453
+// https://rdrr.io/cran/CVD/man/dichromaticCopunctalPoints.html
 
 // Enum-like object that defines the copunctal points of the different color vision deficiencies.
 // TypeScript doesn't allow enum values other than number and string
 export const CopunctalPoint: Record<'DEUTAN' | 'PROTAN' | 'TRITAN', Vector2D> =
   {
-    DEUTAN: [1.08, -0.8],
-    PROTAN: [0.747, 0.253],
-    TRITAN: [0.171, 0],
+    DEUTAN: [1.4, -0.4],
+    PROTAN: [0.7465, 0.2535],
+    TRITAN: [0.1748, 0],
   }
 
 // Red, green, and blue in the xyY color space - source: https://en.wikipedia.org/wiki/SRGB
